2f98e9ae59dbbaa17c8776aa72fd2d3286bf11cc
Mac Book Pro <macbookpro@MacBook-Pro-de-Mac.local>
2018-04-05 13:29:35 -0500

    inicial

---
+++ Map_new_new/js/index.js
@@ -0,0 +1,23 @@
+var qromap = document.getElementById("qro-map"),
+	provinceInfo = document.getElementById("provinceInfo"),
+	allProvinces = qromap.querySelectorAll("g");
+	qromap.addEventListener("click", function(e){ 
+		var province = e.target.parentNode;
+		if(e.target.nodeName == "path") {
+		for (var i=0; i < allProvinces.length; i++) {
+			allProvinces[i].classList.remove("active");
+		}
+		province.classList.add("active");
+		var provinceName = province.querySelector("title").innerHTML,
+		provincePara = province.querySelector("desc p");
+		sourceImg = province.querySelector("img"),
+		imgPath = //https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/";
+		provinceInfo.innerHTML = "";
+		provinceInfo.insertAdjacentHTML("afterbegin", "<img src="+imgPath + sourceImg.getAttribute('xlink:href')+" alt='"+sourceImg.getAttribute('alt')+"'><h1>"+provinceName+"</h1><p>"+provincePara.innerHTML+"</p>");
+		provinceInfo.classList.add("show");
+		}
+	})
+
+
+
+	
